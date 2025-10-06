10bba4d1d0693b1fdeee2ee4b0f480fd60bc765d
maurilio <maurilio.ssemeao@gmail.com>
2025-10-06 11:03:21 -0300

    Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Department dp = new Department(1, "Books");
+
+        System.out.println(dp);
 
     }
 }
