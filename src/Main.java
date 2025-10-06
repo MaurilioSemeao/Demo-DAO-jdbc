6d067b21c706fa0e326266cb561bc4d71651640e
maurilio <maurilio.ssemeao@gmail.com>
2025-10-06 11:13:29 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import model.entities.Department;
+import model.entities.Seller;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
         Department dp = new Department(1, "Books");
 
-        System.out.println(dp);
+        Seller seller = new Seller(21, "Bob", "bob.gmail.com", new Date(), 3000.00, dp );
+
+        System.out.println(seller);
 
     }
 }
